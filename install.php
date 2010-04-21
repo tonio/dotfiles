f76007ec77aa75ae57e1836524d34626a737faa8
Antoine Abt <me@abt.im>
2010-04-21 15:06:39 +0200

    global exclude file for git

--- install.php
+++ install.php
@@ -1,15 +1,9 @@
 #!/usr/bin/env php
 <?php
 
-$excludes = array(
-  '.gitignore',
-  '.git',
-  'install.php'
-);
-
 foreach (new DirectoryIterator('./') as $item) {
   if (
-    in_array($item, $excludes) || 
+    in_array($item, array('.git', 'install.php')) ||
     $item->isDot() ||
     preg_match('/\.sw(p|o)$/', $item->getFileName())
   ) continue;
