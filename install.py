2b74969d954c7810ddc60badfbfc116f753acb34
Antoine Abt <me@abt.im>
2010-05-19 14:08:58 +0200

    better userdir retrieving, thx bbinet

--- install.py
+++ install.py
@@ -5,7 +5,7 @@ exclude = [ '*.sw*', '.git', 'install.*' ]
 
 for f in os.listdir('.'):
     if not any(fnmatch.fnmatch(f, p) for p in exclude):
-        path = os.getenv('HOME')+'/'+f
+        path = os.path.join(os.path.expanduser('~'), f)
         if os.path.isfile(path) or os.path.isdir(path):
             os.unlink(path)
         os.symlink(os.path.abspath(f), path)
